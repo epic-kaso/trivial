707e25e54754dce527d53c3ec69fae68b4d205bf
Akachukwu Okafor <kasoprecede47@gmail.com>
2015-01-05 16:39:49 +0100

    fixed mp3 mime type

--- app/views/pages/welcome.blade.php
+++ app/views/pages/welcome.blade.php
@@ -8,13 +8,23 @@
                     <div class="side-menu">
                         <ul class="nav nav-stacked">
                             <li>
-                                <a href="{{ route('user.storage') }}">
-                                    <span class="fa fa-cloud pull-left"></span> Buy Storage Space
+                                <a href="{{ route('user.storage') }}" class="active">
+                                    <span class="fa fa-folder pull-left"></span> All Files
                                 </a>
                             </li>
                             <li>
                                 <a href="{{ route('user.storage') }}">
                                     <span class="fa fa-cc pull-left"></span> Files For Sale
+                                </a>
+                            </li>
+                        </ul>
+
+                        <div style="margin-top: 50px;"></div>
+
+                        <ul class="nav nav-stacked">
+                            <li>
+                                <a href="{{ route('user.storage') }}">
+                                    <span class="fa fa-cloud pull-left"></span> Buy Storage Space
                                 </a>
                             </li>
                         </ul>
@@ -61,15 +71,15 @@
                                                         <a class="list-group-item"
                                                            href="{{ route('sell.show',[$file->hashcode]) }}"
                                                            data-placement="top" data-toggle="tooltip">
-                                                            <span class="glyphicon glyphicon-share-alt"></span>
-                                                            Share & Sale
+                                                            <span class="glyphicon glyphicon-link"></span>
+                                                            Sale Link
                                                         </a>
                                                     @else
                                                         <a data-placement="top" data-toggle="tooltip"
                                                            class="list-group-item modal-toggle" href="#shareSellModal"
                                                            data-action="{{ route('user.files.enable-share-sell',[$file->id]) }}">
                                                             <span class="glyphicon glyphicon-share-alt"></span>
-                                                            Enable Share & Sale
+                                                            Sale File
                                                         </a>
                                                     @endif
                                                     <a href="" class="list-group-item"
