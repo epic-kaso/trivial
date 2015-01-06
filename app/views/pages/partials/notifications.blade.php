e04376a2782bdd6d3ff71fd9703a939e4d070961
Akachukwu Okafor <kasoprecede47@gmail.com>
2015-01-06 19:33:12 +0100

    Version 1 fully working

--- app/views/pages/partials/notifications.blade.php
+++ app/views/pages/partials/notifications.blade.php
@@ -1,30 +1,45 @@
 @if(!empty(Session::get('message')))
-    <div class="alert alert-info alert-fixed-top text-center">
+    <div class="alert alert-info alert-fixed-top text-center alert-dismissible" role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
         <p>{{ Session::get('message') }}</p>
     </div>
 @endif
 
 @if(!empty(Session::get('status')))
-    <div class="alert alert-info alert-fixed-top text-center">
+    <div class="alert alert-info alert-fixed-top text-center alert-dismissible" role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
         <p>{{ Session::get('status') }}</p>
     </div>
 @endif
 
 
 @if(!empty(Session::get('success')))
-    <div class="alert alert-success alert-fixed-top text-center">
+    <div class="alert alert-success alert-fixed-top text-center alert-dismissible" role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
         <p>{{ Session::get('success') }}</p>
     </div>
 @endif
 
 @if(!empty(Session::get('error')))
-    <div class="alert alert-danger alert-fixed-top text-center">
+    <div class="alert alert-danger alert-fixed-top text-center alert-dismissible" role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
         <p>{{ Session::get('error')}}</p>
     </div>
 @endif
 
 @if(count($errors->all()) > 0)
-    <div class="alert alert-danger alert-fixed-top text-center">
+    <div class="alert alert-danger alert-fixed-top text-center alert-dismissible" role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
         @foreach($errors->all() as $error)
             @if(isset($error))
                 <p>{{ $error }}</p>
