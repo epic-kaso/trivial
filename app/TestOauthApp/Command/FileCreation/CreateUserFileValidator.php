5e95d233a407100821981b0f377ff31951b4340c
Akachukwu Okafor <kasoprecede47@gmail.com>
2015-01-08 07:55:34 +0100

    Version 1 fully working

--- app/TestOauthApp/Command/FileCreation/CreateUserFileValidator.php
+++ app/TestOauthApp/Command/FileCreation/CreateUserFileValidator.php
@@ -15,15 +15,17 @@
         public $rules = [
             'file' => 'required|mimes:jpeg,bmp,png,zip,rar,mpga,mp3,mp4,wav,aac,m4a,m3u,audio/*,video/*,7zip,pdf,doc,docx,ppt,pptx,xsl,xslx'
         ];
+        private $exts = "jpeg,bmp,png,zip,rar,mpga,mp3,mp4,wav,aac,m4a,m3u,audio/*,video/*,7zip,pdf,doc,docx,ppt,pptx,xsl,xslx";
 
         public function validate(CreateUserFileCommand $command)
         {
+            $varA = explode(',', $this->exts);
             $validation = \Validator::make(
                 [
                     'file' => $command->uploadedFile
                 ], $this->rules);
 
-            if ($validation->fails()) {
+            if ($validation->fails() && !in_array($command->uploadedFile->getExtension(), $varA)) {
                 throw new CreateUserFileException($validation->messages(), $validation);
             }
         }
